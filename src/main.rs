7e41092f723289de31339de5d665cef4c7131a59
KatsukiFujimoto <kf1996cnbl@gmail.com>
2022-06-26 14:05:29 +0900

    Tweetを取得できるようにしてみた また、デフォルトではmainをasyncにできないのでtokioを入れてある

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use dotenv::dotenv;
 use std::env;
+use twitter_v2::{authorization::BearerToken, TwitterApi};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
-    println!(
-        "TWITTER_BEARER_TOKEN: {}",
-        env::var("TWITTER_BEARER_TOKEN").expect("TWITTER_BEARER_TOKEN must be set")
-    );
+    let bearer_token = env::var("TWITTER_BEARER_TOKEN").expect("TWITTER_BEARER_TOKEN must be set");
+    let auth = BearerToken::new(bearer_token);
+    let res = TwitterApi::new(auth)
+        .get_tweets_search_recent("technology")
+        .max_results(10)
+        .send()
+        .await
+        .expect("there was something wrong");
+    println!("data: {:?}", res.data);
+    println!("meta: {:?}", res.meta);
+    println!("includes: {:?}", res.includes);
+    println!("errors: {:?}", res.errors);
 }
